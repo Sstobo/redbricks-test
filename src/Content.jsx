3eff7fc7a91cdb253571306418e53bdcd39c6b4f
Sstobo <seanstobo@gmail.com>
2024-08-12 15:00:53 -0700

    Toast pops! As expected fortunatly :)

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,10 +6,18 @@ import { useToast } from "@/components/ui/use-toast"
 
 export default function Content() {
   const { toast } = useToast();
+
+  const handleClick = () => {
+    toast({
+      title: "Scheduled: Catch up",
+      description: "Friday, February 12, 2021 at 5:57 PM",
+    });
+  };
+
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
-
+      <button onClick={handleClick}>Show toast</button>
       <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
         TODO: List of liked submissions here (delete this line)
       </Typography>
