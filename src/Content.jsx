15370c9d5ef5d73fcf8983f2fb606376cd0523b2
Sstobo <seanstobo@gmail.com>
2024-08-12 14:59:21 -0700

    Initialized Shadcn toaster and tailwind. After a lot of dependancy issues. Ive managed to enforce some version controls that gave gotten us back in the game

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { useToast } from "@/components/ui/use-toast"
+
 
 export default function Content() {
+  const { toast } = useToast();
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
